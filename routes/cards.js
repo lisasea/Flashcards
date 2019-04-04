88185baef8d2e54bd871f6263f9ffa4fe6248af2
Lisa Carbonell <morekaleplease@gmail.com>
2019-04-04 15:44:49 -0700

    if else if answer question

--- routes/cards.js
+++ routes/cards.js
@@ -12,12 +12,32 @@ const { cards } = data; // eo6 equivalent to const cards = data.cards;
 */
 
 router.get('/:id', (req, res) => { //  " '/:' " - tells express to use this part of the url as a variable
-    res.render('card', { 
+    const { side } = req.query;
+    const { id } = req.params;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+
+/*  is now brlow:
+res.render('card', { 
         prompt: cards[req.params.id].question,
         hint: cards[req.params.id].hint
     });
 });
+*/
 
+    const templateData = { id, text };
 
+    if ( side === 'question' ) {
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateDate.sideToShowDisplay = 'Answer'; //Jennifer - why is this capitalized?
+    } else if ( side === 'answer' ) {
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+
+    }
+
+    res.render('card', templateData);
+});
 
 module.exports = router; //finally need to export the router
